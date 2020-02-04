7a7043ec27db9c38dd5a4145591ced828f21f9e3
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-02-04 15:01:38 -0600

    update ruun.py

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ footer = dbc.Container(
         dbc.Col(
             html.P(
                 [
-                    html.Span('Caleb Spraul', className='mr-2'), 
+                    html.Span('C Spraul', className='mr-2'), 
                     html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:caleb@spraul.net'), 
                     html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/jcs-lambda/unit2-syllables'), 
                     # html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/<you>/'), 
